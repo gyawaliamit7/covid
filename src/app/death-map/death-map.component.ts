744faa7844441d1c3a4fc94e7b4858194792df40
gyawaliamit7 <gyawaliamit7@gmail.com>
2020-04-09 21:59:01 -0500

    Second push

--- src/app/death-map/death-map.component.ts
+++ src/app/death-map/death-map.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { StateMapComponent } from './../state-map/state-map.component';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-death-map',
   templateUrl: './death-map.component.html',
   styleUrls: ['./death-map.component.css']
 })
-export class DeathMapComponent implements OnInit {
+export class DeathMapComponent implements AfterViewInit {
+
+  @ViewChild(StateMapComponent, {static: false})
+  stateMap: StateMapComponent;
+
+
 
   constructor() { }
-
-  ngOnInit() {
+  ngAfterViewInit() {
+    console.log('Checking');
+    console.log(this.stateMap.totalCases);
   }
+
 
 }
